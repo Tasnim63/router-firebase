d0d7c4f496af32c7dace52ad43a85942f3c6bef2
tasnim63 <tamanna202163@gmail.com>
2022-04-09 00:47:00 +0600

    header commited

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/login'>LogIn</Link>
+        <div className='header'>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/login'>LogIn</Link>
+            </nav>
+
         </div>
     );
 };
